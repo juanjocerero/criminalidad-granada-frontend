401cad853c59fb9bfdec0e90bc2a90599473be2e
Juanjo Cerero <jjcerero@ideal.es>
2020-01-21 20:20:35 +0100

    added rudimentary error control flow in crimeviz page

--- src/components/CrimeVisualization.js
+++ src/components/CrimeVisualization.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { css } from '@emotion/core';
 import ClipLoader from 'react-spinners/ClipLoader';
 import axios from 'axios';
@@ -26,38 +26,42 @@ const fetchApiEndpoint = async (url) => {
 };
 
 const CrimeVisualization = () => {
-
+  
   // eslint-disable-next-line no-unused-vars
   const [queryUrl, setQueryUrl] = useState(getDefaultApiEndpointUrl());
   const [state, setState] = useState({ crimenes: [], isLoading: true });
   const [error, setError] = useState(null);
   
   useEffect(() => {
-
+    
     async function fetchData() {
       const queryResponse = await fetchApiEndpoint(queryUrl);
-
+      
       queryResponse.error ? 
       setError(queryResponse.error) : 
       setState({ crimenes: queryResponse.data, isLoading: false });
     }
-
+    
     fetchData();
-
+    
   }, [queryUrl]);
-
+  
   console.log(state.crimenes);
   
-  if (error) {
-    // TODO: crear componente para manejar el error
-    return (<div className="fullscreen text-center">error</div>)
-  }
-  
   return (
-    <div className="fullscreen">
+    <Fragment>
+    <div className="fullscreen align-items-center">
+    
     <ClipLoader css = { cssOverride } size = { 60 } color = { '#b90021' } loading = { state.isLoading } />
-    { state.crimenes.map(item => <li style={ { fontSize: '0.8rem' } } key={item._id}>{ item.titular }</li>) }
+    
+    // TODO: crear componente para manejar el error
+    { error && <div>there was an error here...</div> }
+    
+    { state.crimenes.map(item => <li style={ { fontSize: '0.9rem' } } key={item._id}>{ item.titular }</li>) }
+    
     </div>
+    </Fragment>
+    
     );
     
   };
