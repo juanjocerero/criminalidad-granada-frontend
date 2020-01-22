a0067b332fd8ebc6cb300333449668647f6cfec8
juanjocerero <jjcerero@ideal.es>
2020-01-22 04:35:00 +0100

    there is a map now

--- src/components/CrimeMap.js
+++ src/components/CrimeMap.js
@@ -5,13 +5,23 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import '../css/CrimeMap.scss';
 
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
 const CrimeMap = () => {
   
   const [position, setPosition] = useState([51.505, -0.9]);
   const [zoom, setZoom] = useState(12);
-
+  
+  // https://stackoverflow.com/questions/49441600/react-leaflet-marker-files-not-found
+  let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+  });
+  L.Marker.prototype.options.icon = DefaultIcon;
+  
   return (
-
+    
     <Map center={position} zoom={zoom}>
     <TileLayer
     attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
