4af2f634be566cb63c191eb4539aa7aa7dcae425
Juanjo Cerero <jjcerero@ideal.es>
2020-02-10 15:20:58 +0100

    fixed categories

--- src/Resources/Colors.js
+++ src/Resources/Colors.js
@@ -1,8 +1,9 @@
 export const categoriesColors = {
   'Robo': '#b90021',
+  'Asalto': '#b07672',
   'Asesinato': '#00272b',
   'Delito sexual': '#ff6663',
-  'Estafa': '#e6aa68',
+  'Delito económico': '#e6aa68',
   'Seguridad vial': '#46a2d4',
   'Tráfico de drogas': '#3eb87d',
   'Violencia de género': '#973ec7'
